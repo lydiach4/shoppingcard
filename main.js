09fc5d84efecef15395af8ca2be707dacfb677fe
lydiach4 <lydiachibane2@gmail.com>
2024-10-09 09:47:42 +0200

    shopping card

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
 const totalprice = document.getElementById('total-price');
 const cartitems = document.getElementById('cart-items');
 const itemCard = document.getElementsByClassName('item-card');
-const itemsnumber = 0 ; 
+ 
 for (let itemcard of itemCard ){
     const minusbutton = itemcard.querySelector(".minus-quantity-button");
     const productprice = itemcard.querySelector(".item-price");
     const addbutton = itemcard.querySelector(".shop-now");
     const itemname = itemcard.querySelector('.item-name');
+    const itemsquantity = itemcard.querySelector('.items-quantity')
+    
     addbutton.onclick = () => {
+      itemsquantity.innerText ++ ;
         totalprice.innerText = parseInt(totalprice.innerText) + parseInt(productprice.innerText);
-        cartitems.innerHTML += `<li class="text-black">${itemname.innerText} </li>`; 
+        cartitems.innerHTML += `<li">${itemname.innerText} </li>`; 
         itemsnumber ++ ; 
     
        
     }
     minusbutton.onclick = () => {
-        if (totalprice.innerText > 0) {
-         
+
+     
+        if (parseInt(itemsquantity.innerText) > 0) {
+          itemsquantity.innerText -- ;
           totalprice.innerText = parseInt(totalprice.innerText) - parseInt(productprice.innerText);
          
         }
